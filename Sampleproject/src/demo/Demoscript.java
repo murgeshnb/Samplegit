edd5f513e59fd0020c61dc0b26348b8501956adc
Dattu V Kulkarni <Dattu V Kulkarni@LAPTOP-P09V1KSC>
2022-01-15 12:37:06 +0530

    My first commit

---
+++ Sampleproject/src/demo/Demoscript.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demoscript {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("1",true);
+	}
+
+}
